f035017144b3d98ac9d98218c94d9a204af5aca3
Su Wei Li <134115566+Houberyou@users.noreply.github.com>
2023-12-08 12:05:46 +0000

    Update entrypoint.sh

--- files/entrypoint.sh
+++ files/entrypoint.sh
@@ -2,7 +2,7 @@
 
 # 设置各变量
 WSPATH=${WSPATH:-'argo'}
-UUID=${UUID:-'de04add9-5c68-8bab-950c-08cd5320df18'}
+UUID=${UUID:-'5c14f561-dc7d-4828-ad2a-2f0b228687a0'}
 WEB_USERNAME=${WEB_USERNAME:-'admin'}
 WEB_PASSWORD=${WEB_PASSWORD:-'password'}
 
